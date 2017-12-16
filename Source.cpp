27a63158e9ff50243f87196553036f8b603e20bb
David Baari <foxxbusiness@hotmail.com>
2017-12-16 21:41:00 +0100

    implementacia cmp do main_source

--- Source.cpp
+++ Source.cpp
@@ -13,6 +13,23 @@ typedef struct bazar{
 	struct bazar *next;
 } BAZAR;
 
+int cmp(char find[50], char source[50]){
+	char low_source[50];
+	char low_find[50];						//vlastna funkcia ktora mi hlada v stringoch zadany substring a ignoruje velke/male pismena
+
+	strcpy((low_source),source);	
+	strcpy((low_find),find);
+	strlwr(low_source);						//string aj substing si zmenim na male pismena aby som ich mohol porovnat pomocou strcpy ktory hlada vyskyt substringu
+	strlwr(low_find);
+
+	if(strstr(low_source,low_find) != NULL){
+		return 1;
+	}											//funkcia vracia hodnotu 1 pokial tam substring je , inak vracia 0
+	else
+		return 0;
+
+}
+
 
 void nacitaj(FILE **fr, int *list_amount, BAZAR **head, BAZAR **current,BAZAR **tail,int *n_fun_done){
 	char h,row[50],stav[200];
